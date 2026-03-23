e4b3b7993785aab4f7de65623a247a28535d04e5
pauloalarico <paulocraft20@gmail.com>
2026-03-23 01:06:42 -0300

    refactor: change writer for double writing

--- src/main/java/com/example/demo/infra/batch/config/DataWriterConfig.java
+++ src/main/java/com/example/demo/infra/batch/config/DataWriterConfig.java
@@ -1,17 +1,38 @@
 package com.example.demo.infra.batch.config;
 
 import com.example.demo.domain.model.Payment;
-import jakarta.persistence.EntityManagerFactory;
 import org.springframework.batch.infrastructure.item.ItemWriter;
-import org.springframework.batch.infrastructure.item.database.JpaItemWriter;
+import org.springframework.batch.infrastructure.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DataWriterConfig {
 
     @Bean
-    public ItemWriter<Payment> writer(EntityManagerFactory factory) {
-        return new JpaItemWriter<>(factory);
+    public ItemWriter<Payment> writerSuccess(DataSource dataSource) {
+        return new JdbcBatchItemWriterBuilder<Payment>()
+                .dataSource(dataSource)
+                .sql("""
+                        INSERT INTO payments (id, client_id, client_name, value, due_date, status) 
+                        VALUES (:id, :clientId, :clientName, :value:, :dueDate, :status)
+                        """)
+                .beanMapped()
+                .build();
     }
+
+    @Bean
+    public ItemWriter<Payment> writerError(DataSource dataSource) {
+        return new JdbcBatchItemWriterBuilder<Payment>()
+                .dataSource(dataSource)
+                .sql("""
+                        INSERT INTO dead_payments_table (id, client_id, client_name, value, due_date, status) 
+                        VALUES (:id, :clientId, :clientName, :value:, :dueDate, :status)
+                        """)
+                .beanMapped()
+                .build();
+    }
+
 }
