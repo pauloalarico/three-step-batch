16353b25c6d8002b51d451d805ea188b0daf6268
pauloalarico <paulocraft20@gmail.com>
2026-03-22 23:58:03 -0300

    feat: add the Reader and the env config for the input

---
+++ src/main/java/com/example/demo/infra/batch/config/CsvFileReaderConfig.java
@@ -0,0 +1,24 @@
+package com.example.demo.infra.batch.config;
+
+import com.example.demo.domain.model.Payment;
+import org.springframework.batch.infrastructure.item.ItemReader;
+import org.springframework.batch.infrastructure.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+
+@Configuration
+public class CsvFileReaderConfig {
+
+    @Value("${app.resource}")
+    private String inputResource;
+
+    public ItemReader<Payment> reader() {
+        return new FlatFileItemReaderBuilder<Payment>()
+                .resource(new FileSystemResource(inputResource))
+                .delimited()
+                .delimiter(",")
+                .names("id", "clientId", "clientName", "value", "dueDate", "status")
+                .build();
+    }
+}
