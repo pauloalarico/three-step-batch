666b6ef95d017427265be2c601e99e92007a2d62
pauloalarico <paulocraft20@gmail.com>
2026-03-23 23:39:45 -0300

    refactor: add Data annotation for the entity Payment

--- src/main/java/com/example/demo/domain/model/Payment.java
+++ src/main/java/com/example/demo/domain/model/Payment.java
@@ -3,11 +3,13 @@ package com.example.demo.domain.model;
 import com.example.demo.domain.enums.PaymentStatus;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Entity
+@Data
 public class Payment {
 
     @Id
