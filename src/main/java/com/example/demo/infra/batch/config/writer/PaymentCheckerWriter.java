9dd7b66adbeffe0c7dbfe855b05c064a8c64b068
pauloalarico <paulocraft20@gmail.com>
2026-03-26 00:34:00 -0300

    feat: add a thread safe writer thats gonna check and persists in two table for two writers

---
+++ src/main/java/com/example/demo/infra/batch/config/writer/PaymentCheckerWriter.java
@@ -0,0 +1,38 @@
+package com.example.demo.infra.batch.config.writer;
+
+import com.example.demo.domain.model.ProcessedPayment;
+import org.springframework.batch.infrastructure.item.*;
+
+import java.util.List;
+
+public class PaymentCheckerWriter implements ItemWriter<ProcessedPayment> {
+
+    private final ItemWriter<ProcessedPayment> writerSuccess;
+
+    private final ItemWriter<ProcessedPayment> writerError;
+
+    public PaymentCheckerWriter(ItemWriter<ProcessedPayment> writerSuccess, ItemWriter<ProcessedPayment> writerError) {
+        this.writerSuccess = writerSuccess;
+        this.writerError = writerError;
+    }
+
+    @Override
+    public void write(Chunk<? extends ProcessedPayment> chunk) throws Exception {
+        List<? extends ProcessedPayment> success = chunk.getItems().stream()
+                .filter(ProcessedPayment::processed)
+                .toList();
+
+        List<? extends ProcessedPayment> errors = chunk.getItems().stream()
+                .filter(p-> !p.processed())
+                .toList();
+
+        if(!success.isEmpty()) {
+            writerSuccess.write(new Chunk<>(success));
+        }
+
+        if(!errors.isEmpty()) {
+            writerError.write(new Chunk<>(errors));
+        }
+    }
+
+}
