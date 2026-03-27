d9283e14317f8589860b06a9214a9bfe20237a6b
pauloalarico <paulocraft20@gmail.com>
2026-03-27 02:53:23 -0300

    refactor: delete the jpa hibernate implementations

--- src/main/java/com/example/demo/domain/repository/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.domain.repository;
-
-import com.example.demo.domain.model.Payment;
-
-public interface PaymentRepository {
-
-    Payment save(Payment payment);
-
-}
--- src/main/java/com/example/demo/infra/persistence/repository/PaymentRepositoryIn.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.infra.persistence.repository;
-
-import com.example.demo.domain.model.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepositoryIn extends JpaRepository<Payment, Long> {
-}
