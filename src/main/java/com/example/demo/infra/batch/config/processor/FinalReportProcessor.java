0fe929f30e19a497f47faf77b2d463134af67b56
pauloalarico <paulocraft20@gmail.com>
2026-03-30 03:29:24 -0300

    feat: add the process to convert the payment to FinalReport

---
+++ src/main/java/com/example/demo/infra/batch/config/processor/FinalReportProcessor.java
@@ -0,0 +1,25 @@
+package com.example.demo.infra.batch.config.processor;
+
+import com.example.demo.domain.model.FinalReport;
+import com.example.demo.domain.model.Payment;
+import lombok.extern.slf4j.Slf4j;
+import org.jspecify.annotations.Nullable;
+import org.springframework.batch.infrastructure.item.ItemProcessor;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class FinalReportProcessor implements ItemProcessor<Payment, FinalReport> {
+
+    @Override
+    public @Nullable FinalReport process(Payment item) throws Exception {
+
+        return new FinalReport(
+                item.getClientId(),
+                item.getClientName(),
+                item.getValue(),
+                item.getValueWithTax()
+        );
+    }
+
+}
