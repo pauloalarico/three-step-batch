c71cbfc403f12cf47e6caa9777c172e991654c3b
pauloalarico <paulocraft20@gmail.com>
2026-03-30 03:30:26 -0300

    feat: add the file writer for report

---
+++ src/main/java/com/example/demo/infra/batch/config/writer/finalreport/FinalReportWriter.java
@@ -0,0 +1,29 @@
+package com.example.demo.infra.batch.config.writer.finalreport;
+
+import com.example.demo.domain.model.FinalReport;
+import org.springframework.batch.infrastructure.item.file.FlatFileItemWriter;
+import org.springframework.batch.infrastructure.item.file.builder.FlatFileItemWriterBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+
+@Configuration
+public class FinalReportWriter {
+
+    @Value("${app.output}")
+    private String outputResource;
+
+    @Bean
+    public FlatFileItemWriter<FinalReport> writer() {
+        return new FlatFileItemWriterBuilder<FinalReport>()
+                .name("final-report-file")
+                .resource(new FileSystemResource(outputResource + "final_report.csv"))
+                .delimited()
+                .delimiter(",")
+                .names(new String[] {"clientId", "clientName", "originalValue", "totalWithTax"})
+                .headerCallback(w -> w.write("client_id, nm_client, original_payment_value, total_payment_value"))
+                .build();
+
+    }
+}
