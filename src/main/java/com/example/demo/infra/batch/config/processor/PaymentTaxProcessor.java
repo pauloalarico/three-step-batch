2e3897bb95b907767eaabedc605dc00ce737b9cf
pauloalarico <paulocraft20@gmail.com>
2026-03-31 00:00:49 -0300

    refactor: adapt the processor for usage of the interface

--- src/main/java/com/example/demo/infra/batch/config/processor/PaymentTaxProcessor.java
+++ src/main/java/com/example/demo/infra/batch/config/processor/PaymentTaxProcessor.java
@@ -1,17 +1,27 @@
 package com.example.demo.infra.batch.config.processor;
 
-import com.example.demo.application.service.TaxCalculator;
 import com.example.demo.domain.model.Payment;
+import com.example.demo.domain.servjce.TaxPolicy;
+import lombok.RequiredArgsConstructor;
 import org.springframework.batch.infrastructure.item.ItemProcessor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class PaymentTaxProcessor implements ItemProcessor<Payment, Payment> {
+
+    private final TaxPolicy taxPolicy;
 
     @Override
     public Payment process(Payment item) throws Exception {
-        var taxProcessed = TaxCalculator.calculateTax(item.getValue(), item.getDueDate());
-        item.applyValue(taxProcessed);
+
+        /*TODO
+        AQUI EU VOU FAZER UM NOVO OBJETO PARA PAGAMETNO, COMO EU TENHO UM CENÁRIO DE CONCORRÊNCIA; NÃO É EXATAMENTE THREADSAFE
+        A SOLUÇÃO É DEIXAR PAYMENT INTOCÁVEL E CRIAR UM OBJETO COM O PAGAMENTO COM TAXA REPROCESSADO
+
+         */
+        var taxProcessed = taxPolicy.calculateTax(item.getValue(), item.getDueDate());
+        item.applyOverdueTax(taxProcessed);
 
         return item;
 
